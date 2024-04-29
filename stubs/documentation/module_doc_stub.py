a338a20b6ba20e29b8bfd3bf4fbc939f78812ee3
devinci-it <vince.it@icloud.com>
2024-04-29 02:10:35 -0700

    Merged changes from 'wip' branch for 'stubs' directory

---
+++ stubs/documentation/module_doc_stub.py
@@ -0,0 +1,13 @@
+# Module: {{module_name}}
+
+"""
+Description of the module.
+
+Attributes:
+    attribute1 (type): Description of attribute1.
+    attribute2 (type): Description of attribute2.
+
+Classes:
+    ClassName1: Description of ClassName1.
+    ClassName2: Description of ClassName2.
+"""
---
+++ stubs/wiki/project_doc_stub.py
@@ -0,0 +1,34 @@
+"""
+
+Project Name
+=============
+
+Description:
+------------
+Brief description of the project.
+
+Features:
+---------
+{{feature_list}}
+
+Dependencies:
+-------------
+{{dependency_list}}
+
+Directory Structure:
+--------------------
+{{tree-output}}
+
+Usage:
+------
+How to use the project, including examples if applicable.
+
+Classes:
+--------
+{{class_list}}
+
+Functions:
+----------
+{{function_list}}
+
+"""
